5a7f0add496afe1c21c29d8dc8aef0db65a441d2
preetichandrasekaran <preetichandrasekaran@gmail.com>
2023-11-07 20:36:40 +0530

    message

---
+++ src/test/java/org/test/Hooks.java
@@ -0,0 +1,29 @@
+package org.test;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+import io.cucumber.core.api.Scenario;
+import io.cucumber.java.After;
+
+public class Hooks  {
+
+//@Before
+//public void launchBrowser() {
+	
+//}
+WebDriver driver;
+@After
+public void ss(Scenario s) {
+	
+	if (s.isFailed()) {
+		
+		TakesScreenshot t = (TakesScreenshot)driver;
+		byte[] snap = t.getScreenshotAs(OutputType.BYTES);
+		s.embed(snap, "image/png");
+	}
+}
+}
+
+
